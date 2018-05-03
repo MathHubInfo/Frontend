9d56d47aabb00832205e3d0e466a9d8720babe9c
AiraComet <jojo-see@web.de>
2018-05-03 17:39:20 +0200

    Added logo images

--- src/components/fragments/footer.tsx
+++ src/components/fragments/footer.tsx
@@ -1,47 +1,77 @@
 import * as React from "react";
 
-import { Container, Divider, Grid, Header, List, Segment } from "semantic-ui-react";
+import { Container, Divider, Grid, Header, Image, /*List,*/ Segment } from "semantic-ui-react";
 
 import { IMathHubContext, WithContext} from "../../context";
 
 export const Footer = WithContext((context: IMathHubContext) => class extends React.Component {
   public render() {
       return (
-        <Segment inverted vertical style={{ margin: "5em 0em 0em", padding: "5em 0em" }}>
-          <Container textAlign="center">
+        <Segment color="grey" inverted vertical style={{ margin: "5em 0em 0em", padding: "5em 0em"}}>
+          <Container textAlign="left">
             <Grid divided inverted stackable>
               <Grid.Row>
-                <Grid.Column width={3}>
-                  <Header inverted as="h4" content="Group 1" />
-                  <List link inverted>
-                    <List.Item as="a">Link One</List.Item>
-                    <List.Item as="a">Link Two</List.Item>
-                    <List.Item as="a">Link Three</List.Item>
-                    <List.Item as="a">Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <Header inverted as="h4" content="Group 2" />
-                  <List link inverted>
-                    <List.Item as="a">Link One</List.Item>
-                    <List.Item as="a">Link Two</List.Item>
-                    <List.Item as="a">Link Three</List.Item>
-                    <List.Item as="a">Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <Header inverted as="h4" content="Group 3" />
-                  <List link inverted>
-                    <List.Item as="a">Link One</List.Item>
-                    <List.Item as="a">Link Two</List.Item>
-                    <List.Item as="a">Link Three</List.Item>
-                    <List.Item as="a">Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <Header inverted as="h4" content="Footer Header" />
-                  <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-                </Grid.Column>
+                <Grid.Column width={4}>
+                  <Header as="h4" content="Developed by:" />
+                  <Image
+                                size="tiny"
+                                src={require("../../../assets/logos/kwarc_logo.png")}
+                                style={{ marginRight: "1.5em" }}
+                                alt="MathHub Logo"
+                                href={"https://kwarc.info/"}
+                  />
+                  </Grid.Column>
+                <Grid.Column width={5}>
+                  <Header as="h4" content="Institutions:" />
+                  <Image
+                                size="tiny"
+                                src={require("../../../assets/logos/fau_logo.png")}
+                                style={{ marginRight: "1.5em" }}
+                                alt="FAU Logo"
+                                inline={true}
+                                href={"https://www.fau.eu/"}
+                  />
+                  <Image
+                                size="tiny"
+                                src={require("../../../assets/logos/odk_logo.png")}
+                                style={{ marginRight: "1.5em" }}
+                                alt="ODK Logo"
+                                inline={true}
+                                href={"http://opendreamkit.org/"}
+                  />
+                  <Image
+                                size="tiny"
+                                src={require("../../../assets/logos/jacobs_logo.png")}
+                                style={{ marginRight: "1.5em" }}
+                                alt="Jacobs University Logo"
+                                inline={true}
+                                href={"https://www.jacobs-university.de/"}
+                  />
+                  </Grid.Column>
+                <Grid.Column width={5}>
+                  <Header as="h4" content="Funding" />
+                  <Image
+                                size="tiny"
+                                src={require("../../../assets/logos/eu_logo.png")}
+                                style={{ marginRight: "1.5em" }}
+                                alt="EU Logo"
+                                inline={true}
+                  />
+                  <Image
+                                size="tiny"
+                                src={require("../../../assets/logos/leibniz_logo.png")}
+                                style={{ marginRight: "1.5em" }}
+                                alt="leibniz Logo"
+                                inline={true}
+                  />
+                  <Image
+                                size="tiny"
+                                src={require("../../../assets/logos/dfg_logo.png")}
+                                style={{ marginRight: "1.5em" }}
+                                alt="dfg University Logo"
+                                inline={true}
+                  />
+                  </Grid.Column>
               </Grid.Row>
             </Grid>
 
