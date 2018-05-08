fc66d45044ebe0eb03395fc1e97f0e49332eccd9
Tom Wiesing <tkw01536@gmail.com>
2018-05-08 18:50:22 +0200

    Update menu and template
    Added Applications- and Help drop down Menu with examples.  Added new
    routes for those examples.  Changed the grey block of the footer to a
    nice line.

--- src/components/fragments/header.tsx
+++ src/components/fragments/header.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { Container, Image, Menu } from "semantic-ui-react";
+import { Container, Dropdown, Image, Menu } from "semantic-ui-react";
 import { Nav } from "../../components/common/nav";
 
 export class Header extends React.Component<{}, {}> {
@@ -17,8 +17,22 @@ export class Header extends React.Component<{}, {}> {
                             />
                             MathHub
                         </Menu.Item>
+                        <Dropdown text="Applications" className="link item">
+                            <Dropdown.Menu>
+                                <Dropdown.Item as={Nav} exact to="/applications/glossary">glossary</Dropdown.Item>
+                                <Dropdown.Item as={Nav} exact to="/applications/dictionary">
+                                    math dictionary
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                         <Menu.Item as={Nav} exact to="/about">About</Menu.Item>
                         <Menu.Item as={Nav} exact to="/legal">Legal</Menu.Item>
+                            <Dropdown text="Help" className="link item">
+                                <Dropdown.Menu>
+                                    <Dropdown.Item as={Nav} exact to="/help/helpone">Example1</Dropdown.Item>
+                                    <Dropdown.Item as={Nav} exact to="/help/helptwo">Example2</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
                     </Container>
             </Menu>
             );
---
+++ src/routes/applications/dictionary.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export class Dictionary extends React.Component<{}, {}> {
+    public render() {
+        return (
+            <div>
+                    Here will be the math dictionary.
+            </div>
+        );
+    }
+}
