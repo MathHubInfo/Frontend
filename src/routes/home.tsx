35abea2d123a3c61e5e752aa890ff1cc1018725c
AiraComet <jojo-see@web.de>
2018-06-05 00:15:40 +0200

    updated Home

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,34 +1,59 @@
 import * as React from "react";
 
-import { Card, Container, Divider } from "semantic-ui-react";
-
+import { Container, Divider, Image } from "semantic-ui-react";
+import { LoadWithPromise } from "../components/common/lazy";
 import { Nav } from "../components/common/nav";
+import { MHTitle } from "../utils/title";
 
 export class Home extends React.Component<{}, {}> {
+    constructor(props: {}) {
+        super(props);
+        this.loadContent = this.loadContent.bind(this);
+    }
+
+    private getHomeText() { return import("../../HOME.txt").then((m) => m.default); }
+    private loadContent() { return Promise.all([this.getHomeText()]); }
+
     public render() {
         return (
             <div>
-                <Container text>
-                    Something something home
-                </Container>
+                <LoadWithPromise promise={this.loadContent} title="HOME">{
+                    ([h]) => <HomeDisplay content={h} />}
+                </LoadWithPromise>
                 <Divider />
-                <Container>
-                    <Card>
-                        <Card.Content>
-                            <Card.Header as={Nav} to={`/content`} >
-                                <div>
-                                    <p>Library</p>
-                                </div>
-                            </Card.Header>
-                            <Card.Description>
-                                <div>
-                                    <p>The library can be found here</p>
-                                </div>
-                            </Card.Description>
-                        </Card.Content>
-                    </Card>
+                <Container as={Nav} to={`/content`}>
+                    <Image
+                                size={"medium"}
+                                src={require("../../../assets/library.jpg")}
+                                title="MathHub Libraries"
+                                inline={true}
+                    />
+                </Container>
+                <Container as={Nav} to={`/content`}>
+                    MathHub Libraries
+                </Container>
+                <Container href={"https://github.com/MathHubInfo/Documentation/wiki/libraries"}>
+                    provide groups of
+                </Container>
+                <Container href={"https://github.com/MathHubInfo/Documentation/wiki/math-archives"}>
+                    archives
                 </Container>
             </div>
         );
     }
 }
+
+class HomeDisplay extends React.Component<{content: string}> {
+    public render() {
+        const {content} = this.props;
+
+        return (
+            <MHTitle title="Licenses">
+                <Container text>
+                    <div dangerouslySetInnerHTML={{__html: content}}/>
+                    </Container>
+            </MHTitle>
+        );
+    }
+}
+// library image source: https://www.pexels.com/photo/library-university-books-students-12064/
