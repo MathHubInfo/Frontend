a3c1edf122deb02e834fbd0702fc9afc109c02bc
Tom Wiesing <tkw01536@gmail.com>
2018-11-14 14:29:36 +0100

    Title: Allow one or multiple children

--- src/components/fragments/title.tsx
+++ src/components/fragments/title.tsx
@@ -6,7 +6,9 @@ interface ITitleProps {
     children?: React.ReactChild | React.ReactChild[];
 }
 
+/** Renders a set of children with a given title */
 export default function Title(props: ITitleProps) {
-    const {title, ...rprops} = props;
-    return <DocumentTitle title={title ? `${title} | MathHub` : "MathHub"} {...rprops} />;
+    const {title, children} = props;
+    const child = Array.isArray(children) ? <>{children}</> : children;
+    return <DocumentTitle title={title ? `${title} | MathHub` : "MathHub"} children={child} />;
 }
