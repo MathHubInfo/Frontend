2bfd7ad241e479d91079a6d2f36f924927973d60
Tom Wiesing <tkw01536@gmail.com>
2018-11-17 12:50:31 +0100

    Cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,72 @@
-require('@babel/register')({babelrc: true});
-module.exports = require("./build/dev.js");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+
+const resolve = require("path").resolve;
+
+const common = require("./build/common");
+const env = require("./build/env");
+
+module.exports = merge(common, {
+    // heya we're developing, so be friendly and stuff
+    mode: 'development',
+    devtool: 'eval-source-map',
+
+    // for development, we need rules for linting
+    // and for source maps
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/, 
+                enforce: 'pre',
+                use: [{
+                    loader: 'tslint-loader', 
+                    options: {
+                        configFile: resolve(__dirname, 'tslint.json'), 
+                        tsConfigFile: resolve(__dirname, 'tsconfig.json'),
+                        typeCheck: true,
+                        failOnHint: true,
+                    }
+                }]
+            },
+
+            // source-maps only for development
+            {
+                test: /\.js$/,
+                enforce: 'pre', 
+                use: [ 'source-map-loader' ]
+            }
+        ]
+    },
+
+    // add a server for development
+    devServer: {
+        contentBase: false, 
+        publicPath: "/",
+        
+        // if using BrowserRouter
+        historyApiFallback: env.BROWSER_ROUTER !== '""',
+
+        watchOptions: {
+            aggregateTimeout: 1000,
+            ignored: /node_modules/,
+        },
+
+        // hot reloading
+        hot: true, 
+        inline: true, 
+
+        // fewer logging
+        clientLogLevel: "error", 
+        noInfo: false,
+    }, 
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': env,
+        }), 
+        
+        // for development and hot reloading
+        new webpack.HotModuleReplacementPlugin(), 
+        new webpack.NamedModulesPlugin()
+    ]
+});
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,2 +1,55 @@
-require('@babel/register')({babelrc: true});
-module.exports = require("./build/prod.js");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+
+const resolve = require("path").resolve;
+
+const LicenseInfoWebpackPlugin = require("license-info-webpack-plugin").default;
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const common = require("./build/common");
+const env = require("./build/env");
+
+module.exports = merge(common, {
+    mode: "production",
+
+    optimization: {
+        minimizer: [
+            // keep license info in output
+            new UglifyJsPlugin({
+                sourceMap: false, 
+                uglifyOptions: {
+                    output: {
+                        comments: /^\**!|@preserve|@license|@cc_on/
+                    }
+                }
+            }), 
+
+            // make all the css smaller
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+
+    plugins: [
+        // Cleanup the dist folder automatically
+        new CleanWebpackPlugin([resolve(__dirname, "dist")]), 
+
+        // define production variables
+        new webpack.DefinePlugin({
+            'process.env': Object.assign(env, {
+                'NODE_ENV': JSON.stringify('production'), 
+                'BABEL_ENV': JSON.stringify('production')
+            }),
+        }), 
+
+        // licensing stuff
+        new LicenseInfoWebpackPlugin({
+            glob: '{LICENSE,license,License}*'
+        }),
+
+        // and minimize images
+        new ImageminPlugin()
+    ]
+});
