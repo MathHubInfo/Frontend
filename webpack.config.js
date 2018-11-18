cf8ecf3bf15e97fb8229b07c6f6a637d19b16980
Tom Wiesing <tkw01536@gmail.com>
2018-11-19 00:50:49 +0100

    Update config to increase compiltation speed
    This commit updates the build configuration to dramatically improve
    compilation speed. To achieve this, we:
    - limited tsconfig.json type checking to project files (everything not in
    node_modules/)
    - limited error awesomet-typescript-loader reporting to everything not in node_modules/)
    - removed tslint-loader in favor of running tslint via command line once
    before every re-compilation
    
    This reduces the build time for a development build to about 15 seconds,
    and a production build to about 25 seconds. In summary: Very speed, much fast.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const merge = require("webpack-merge");
 
-const resolve = require("path").resolve;
+const WebpackShellPlugin = require('./build/shell');
 
 const common = require("./build/common");
 const env = require("./build/env");
@@ -15,20 +15,6 @@ module.exports = merge(common, {
     // and for source maps
     module: {
         rules: [
-            {
-                test: /\.tsx?$/, 
-                enforce: 'pre',
-                use: [{
-                    loader: 'tslint-loader', 
-                    options: {
-                        configFile: resolve(__dirname, 'tslint.json'), 
-                        tsConfigFile: resolve(__dirname, 'tsconfig.json'),
-                        typeCheck: true,
-                        failOnHint: true,
-                    }
-                }]
-            },
-
             // source-maps only for development
             {
                 test: /\.js$/,
@@ -61,6 +47,10 @@ module.exports = merge(common, {
     }, 
 
     plugins: [
+        new WebpackShellPlugin({
+            onBuildStart: ["node_modules/.bin/tslint -c tslint.json -p tsconfig.json"]
+        }),
+
         new webpack.DefinePlugin({
             'process.env': env,
         }), 
