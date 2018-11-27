b5630af043e3edaef9520d0766c72680cc23b87d
Tom Wiesing <tkw01536@gmail.com>
2018-11-27 18:37:56 +0100

    Add better chunk splitting

--- build/common.js
+++ build/common.js
@@ -10,12 +10,14 @@ const resolve = require("path").resolve;
 
 const env = require("./env");
 
+const nodeModulesTest = (m) => new RegExp(`/node_modules/${m}[\\/]`, '');
+
 module.exports = {
     // input / output
     entry: ['babel-polyfill', './src/index.tsx'],
     output: {
         filename: '[name].js',
-        chunkFilename: '[name]-[hash].chunk.js',
+        chunkFilename: '[name]-[contenthash].chunk.js',
         path: resolve(__dirname, "..", "dist"),
         publicPath: env.BROWSER_ROUTER !== '""' ? JSON.parse(env.BROWSER_ROUTER) : undefined,
     },
@@ -71,7 +73,58 @@ module.exports = {
     optimization: {
         runtimeChunk: true, 
         splitChunks: {
-            chunks: 'all'
+            chunks: 'all',
+            minChunks: 1, 
+            cacheGroups: {
+                default: false,
+
+                // semantic-ui-react: every folder under dist/es is it's own submodule
+                semanticUIReact: {
+                    test: nodeModulesTest('semantic-ui-react'),
+                    priority: -5,
+                    name: function(mod) {
+                        const subFolder = mod.resource.match(/\/semantic-ui-react\/dist\/es\/([^\/]*)/);
+                        return `semantic-ui-react-${subFolder[1]}`;
+                    }
+                },
+
+                // all other modules: enforce a module based on the folder
+                nodeModules: {
+                    test: /\/node_modules\//,
+                    enforce: true,
+                    priority: -10,
+                    name: function(mod) {
+                        if (!mod.resource) return;
+                        const modName = mod.resource.match(/\/node_modules\/([^\/]*)/);
+                        return `modules-${modName[1]}`;
+                    }
+                },
+
+                // assets: each asset is a single chunk
+                assets: {
+                    test: /\/assets\//,
+                    priority: -15,
+                    name: function (mod) {
+                        if (!mod.resource) return;
+                        let assetName = mod.resource.match(/\/assets\/(.*)/);
+                        assetName = assetName[1].replace('/', '-').replace('.', '-');
+                        return `asset-${assetName}`;
+                    }
+                },
+
+                // src: each file is a single chunk
+                src: {
+                    test: /\/src\//,
+                    priority: -15,
+                    name: function (mod) {
+                        if (!mod.resource) return;
+                        let srcName = mod.resource.match(/\/src\/(.*)/);
+                        srcName = srcName[1].replace('/', '-').replace('.', '-');
+                        return `src-${srcName}`;
+                    }
+                }
+                
+            }
         }
     }, 
     
