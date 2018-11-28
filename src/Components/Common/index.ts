7db0e492505dc1e40e2ad28de888a07eea79f975
Tom Wiesing <tkw01536@gmail.com>
2018-11-28 22:34:58 +0100

    Export Ribbon from Common

--- src/Components/Common/index.ts
+++ src/Components/Common/index.ts
@@ -3,3 +3,4 @@ export { default as Monospace } from "./Monospace";
 export { default as ScrollToTop } from "./ScrollToTop";
 export { default as DictToSwitch } from "./DictToSwitch";
 export { default as Breadcrumbs } from "./Breadcrumbs";
+export { default as Ribbon } from "./Ribbon";
