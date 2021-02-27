c45b5847376ff8f0a05e7a524addb04e886c8aca
Tom Wiesing <tkw01536@gmail.com>
2021-02-27 14:13:20 +0100

    smglom: Fix cross-language linking

--- src/utils/resolve.ts
+++ src/utils/resolve.ts
@@ -1,6 +1,18 @@
 /** resolve resolves a URL relative to a base */
 export function resolveURL(from: string, to: string): string {
-    // this used to be possible with require("url").resolve
-    // but that got deprecated!
-    return new URL(to, from || undefined).href;
+    // try resolving the 'from' url relative to the current location.
+    // when that doesn't work, don't use a base url!
+    let base: URL | undefined;
+    try {
+        base = new URL(from, process.browser ? location.href : undefined);
+    } catch (e) {
+        base = undefined;
+    }
+
+    // parse the actual URL, and when that fails, return an error
+    try {
+        return new URL(to, base).href;
+    } catch (e) {
+        return to;
+    }
 }
